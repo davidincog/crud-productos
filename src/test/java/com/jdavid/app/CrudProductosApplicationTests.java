baa386f570cefae0e9aabdaa80e3ef8daa7f5f1d
davidincog <113152786+davidincog@users.noreply.github.com>
2022-10-05 21:53:02 -0500

    se agregan los archivos necesarios para el proyecto

---
+++ src/test/java/com/jdavid/app/CrudProductosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jdavid.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudProductosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
